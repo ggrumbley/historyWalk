2be243e6f9255583301504c43aff9caa86559f6a
StumblinGrumblin <gary@sweetlabs.com>
2015-06-16 15:25:16 -0700

    Styled navbar and home page

--- public/js/app.js
+++ public/js/app.js
@@ -31,6 +31,17 @@ app.config([
 app.factory('walks', [function () {
   var o = {
     walks: [
+      {title: "The Internet Meme Collection", walk: [
+        {location: 'Joseph Ducreux', text: 'A portrait from 1793 remixed with archaic rap lyrics. What\'s not to love?'},
+        {location: 'Charlie the Unicorn', text: 'In this odd flash animation the user is treated to a hilarious romp as Charlie the Unicorn goes in search of Candy Mountain.'},
+        {location: 'I Took an Arrow in the Knee', text: 'Adventuring is dangerous, some people have to hang it up after a simple arrow to the knee.'},
+        {location: 'Grape Stomp Lady', text: 'The first work hailing from poorly produced TV. This unfortunate lady eats it.'},
+        {location: 'Star Wars Kid', text: 'This Canadian kid has mad skills.'},
+        {location: 'Numa Numa', text: 'Words fail me.'},
+        {location: 'Leeroy Jenkins', text: 'Leeroy Jenkins makes enemies in the World... of Warcraft.'},
+        {location: 'Double Rainbow', text: 'All the way across the sky! I am so high right now.'},
+        {location: 'Peanut Butter Jelly Time', text: 'I am a ba na na!'}
+      ]},
       {title: "Gary's House", walk: [
         {location: 'Computer Room', text: 'This is where Gary Hacks on things.'},
         {location: 'Bedroom', text: 'This is where Gary sleeps.'},
