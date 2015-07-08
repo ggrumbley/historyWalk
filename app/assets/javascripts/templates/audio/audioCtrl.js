7d2029c55d8fb412478a7bae665b6d30985f418a
StumblinGrumblin <gary@sweetlabs.com>
2015-07-08 11:27:26 -0700

    Audio test page built

--- app/assets/javascripts/templates/audio/audioCtrl.js
+++ app/assets/javascripts/templates/audio/audioCtrl.js
@@ -9,6 +9,14 @@
     ]);
 
     function AudioCtrl($sce) {
-      
-    }
+      this.config = {
+        sources: [
+          {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/audios/videogular.mp3"), type: "audio/mpeg"},
+          {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/audios/videogular.ogg"), type: "audio/ogg"}
+        ],
+        theme: {
+          url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
+        }
+      };
+    };
 })();
