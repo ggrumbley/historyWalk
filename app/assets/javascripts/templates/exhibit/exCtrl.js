aadcea22badf5c334015ba4fac41fe4113568890
StumblinGrumblin <gary@sweetlabs.com>
2015-07-08 12:48:46 -0700

    Videogular audio player added to exhibit pages.

--- app/assets/javascripts/templates/exhibit/exCtrl.js
+++ app/assets/javascripts/templates/exhibit/exCtrl.js
@@ -7,14 +7,25 @@
       '$scope',
       '$stateParams',
       'walks',
+      '$sce',
       ExCtrl
     ]);
 
-  function ExCtrl($scope, $stateParams, walks) {
+  function ExCtrl($scope, $stateParams, walks, $sce) {
     var tour = walks.tours[$stateParams.tourID],
         exhibits = tour.exhibits,
         index = $stateParams.exID;
     $scope.tour = tour;
     $scope.exhibit = exhibits[index];
+
+    this.config = {
+      sources: [
+        {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/audios/videogular.mp3"), type: "audio/mpeg"},
+        {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/audios/videogular.ogg"), type: "audio/ogg"}
+      ],
+      theme: {
+        url: "/videogular-themes-default/videogular.css"
+      }
+    };
   };
 })();
