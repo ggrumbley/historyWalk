802f05eac2e1cefe3682a18f0e4564131634dc46
StumblinGrumblin <gary@sweetlabs.com>
2015-07-08 12:07:42 -0700

    Audio player test successful

--- app/assets/javascripts/templates/audio/audioCtrl.js
+++ app/assets/javascripts/templates/audio/audioCtrl.js
@@ -15,7 +15,7 @@
           {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/audios/videogular.ogg"), type: "audio/ogg"}
         ],
         theme: {
-          url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
+          url: "/videogular-themes-default/videogular.css"
         }
       };
     };
