5ec3b3fd6cf44dfd6b632394ed88918565a9b9a3
StumblinGrumblin <gary@sweetlabs.com>
2015-07-12 12:18:32 -0700

    Back button added to exhibit page.

--- app/assets/javascripts/templates/exhibit/exCtrl.js
+++ app/assets/javascripts/templates/exhibit/exCtrl.js
@@ -13,9 +13,10 @@
   function ExCtrl($scope, $stateParams, walks) {
     var tour = walks.tours[$stateParams.tourID],
         exhibits = tour.exhibits,
+        tourID = $stateParams.tourID,
         index = $stateParams.exID;
     $scope.tour = tour;
     $scope.exhibit = exhibits[index];
-
+    $scope.tourID = tourID;
   };
 })();
