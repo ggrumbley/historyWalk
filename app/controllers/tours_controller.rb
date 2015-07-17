36b3d26569b2c280b28b20514931d9be5a30b191
StumblinGrumblin <gary@sweetlabs.com>
2015-07-17 13:55:10 -0700

    Blank slate.

--- app/controllers/tours_controller.rb
+++
@@ -1,2 +0,0 @@
-# PATCH/PUT /tours/1
-# PATCH/PUT /tours/1.json
--- app/models/tour.rb
+++
@@ -1,6 +0,0 @@
-class Tour
-  include Mongoid::Document
-  field :title, type: String
-  field :cover, type: String
-  embeds_many :exhibits
-end
