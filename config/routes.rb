9e9ee5a79d074ce4b5634aebeff2572269564b9e
StumblinGrumblin <gary@sweetlabs.com>
2015-07-30 21:15:38 -0700

    Routes Example

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root to: 'application#angular'
 
-  resources :tours, defaults: {format: 'json'} do
-    resources :exhibits, defaults: {format: 'json'}
+  resources :tours, defaults: {format: 'json'}, only: [:index, :show] do
+    resources :exhibits, defaults: {format: 'json'}, only: [:index, :show]
+  end
+
+  namespace :admin do
+    resources :tours, :exhibits
   end
 
   get '*path' => 'application#angular'
